ac0cb465e266d4d86f85be66a617d7f2bed93561
Siprachanh <sipra.chant@gmail.com>
2022-04-07 16:28:17 -0500

    Created details button and functionalities for animals

---
+++ src/components/animal/AnimalDetail.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { getAnimalById } from '../../modules/AnimalManager';
+import './AnimalDetail.css';
+import { useParams, useNavigate } from "react-router-dom"
+
+export const AnimalDetail = () => {
+  const [animal, setAnimal] = useState({ name: "", breed: "" });
+  //empty object
+
+  const {animalId} = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
+    console.log("useEffect", animalId)
+    getAnimalById(animalId)
+      .then(animal => {
+        setAnimal(animal);
+      });
+  }, [animalId]);
+
+  return (
+    <section className="animal">
+      <h3 className="animal__name">{animal.name}</h3>
+      <div className="animal__breed">{animal.breed}</div>
+      {/* What's up with the question mark???? See below.*/}
+      <div className="animal__location">Location: {animal.location?.name}</div>
+      <div className="animal__owner">Customer: {animal.customer?.name}</div>
+    </section>
+  );
+}
+
