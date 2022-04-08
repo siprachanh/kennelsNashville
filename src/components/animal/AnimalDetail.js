1fcc214fdc28873df0f795ff05c1f13869ef4769
Siprachanh <sipra.chant@gmail.com>
2022-04-08 10:13:08 -0500

    Added the discharge/delete functionality and isLoading to Animal Details to allow dynamic changefor User

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { getAnimalById } from '../../modules/AnimalManager';
 import './AnimalDetail.css';
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom";
+import { deleteAnimal } from '../../modules/AnimalManager';
 
 export const AnimalDetail = () => {
   const [animal, setAnimal] = useState({ name: "", breed: "" });
   //empty object
-
+  const [isLoading, setIsLoading] = useState(true);
   const {animalId} = useParams();
   const navigate = useNavigate();
 
@@ -15,9 +16,23 @@ export const AnimalDetail = () => {
     console.log("useEffect", animalId)
     getAnimalById(animalId)
       .then(animal => {
-        setAnimal(animal);
-      });
+       setAnimal(animal);
+    // setAnimal({
+    //   name: animal.name,
+    //   breed: animal.breed
+    // });
+    setIsLoading(false);
+  });
+     
   }, [animalId]);
+
+  const handleDelete = () => {
+    //invoke the delete function in AnimalManger and re-direct to the animal list.
+    setIsLoading(true);
+    deleteAnimal(animalId).then(() =>
+      navigate("/animals")
+    );
+  };
 
   return (
     <section className="animal">
@@ -26,7 +41,10 @@ export const AnimalDetail = () => {
       {/* What's up with the question mark???? See below.*/}
       <div className="animal__location">Location: {animal.location?.name}</div>
       <div className="animal__owner">Customer: {animal.customer?.name}</div>
+      <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Discharge
+        </button>
     </section>
   );
-}
+};
 
