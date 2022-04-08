86baa5b0a063a76f10a6d9fd609e292aba8daaa7
Siprachanh <sipra.chant@gmail.com>
2022-04-08 11:47:14 -0500

    Added AnimalForm componenet, Route for showing Form, and btn for New Ani Admit

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { AnimalCard} from "./AnimalCard";
 import {getAllAnimals, getAnimalById, deleteAnimal} from "../../modules/AnimalManager";
+import { useNavigate } from 'react-router-dom';
 
 export const AnimalList = () => {
     //the initial state is an empty array
     const [animals, setAnimals] = useState([]);
+    const navigate = useNavigate();
     const getAnimals = () => {
          getAllAnimals()
         .then(animalsFromAPI => {
@@ -32,15 +34,27 @@ useEffect(() => {
 // Finally we use .map to "loop over" the animals array to show a list of animal cards
   
  return (
+     //add this button above your display of animal cards
+     <>
+        <section className="section-content">
+            <button type="button"
+            className="btn"
+            onClick={() => {navigate("/animals/create")}}>
+      Admit Animal
+        </button>
+    </section>
         <div className="container-cards">
-       {animals.map(animal => <AnimalCard 
-       key={animal.id} 
-       animal={animal}
-       handleDeleteAnimal= {handleDeleteAnimal}/> )}
+            {animals.map(animal => <AnimalCard 
+            key={animal.id} 
+            animal={animal}
+            handleDeleteAnimal= {handleDeleteAnimal}/> )}
         </div>
+        </>
+
     );
 };
-
+//useNav allows us to navigate to url then it will look at route to determine
+//which component to display; can invoke a fn or import helpers to help the navigate fn
 //key prop is unique to each child in a list. this is how Reach keeps track of re-rendering only the things that have changed.
 //handleDelete is invoked in ln 38 and then passed in to ln 21 setAnimals
 
